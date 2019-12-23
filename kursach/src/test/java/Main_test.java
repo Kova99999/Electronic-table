39f5c60325520decdb1580ee82df8620ca9637f6
Kova99999 <59012012+Kova99999@users.noreply.github.com>
2019-12-23 04:39:11 +0300

    Create Main_test.java

---
+++ kursach/src/test/java/Main_test.java
@@ -0,0 +1,44 @@
+import Electronic_Table.Main;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Main_test {
+
+    private Main.database tableBase;
+
+    @Before
+    public void init(){
+        tableBase = new Main().new database(true);
+    }
+
+    @Test
+    public void Test_Size(){
+        assertEquals(0, tableBase.getSize());
+    }
+
+
+    @Test
+    public void Test_Index (){
+        tableBase.createNewTable("abc", 2, 2, "admin");
+        assertEquals(0, tableBase.returnIndex("abc"));
+        tableBase.delete("abc");
+    }
+
+    @Test
+    public void Test_Name(){
+        tableBase.createNewTable("test", 2, 2, "admin");
+        assertEquals("test", tableBase.getTableName(0));
+        tableBase.delete("test");
+    }
+
+    @Test
+    public void Test_contains() {
+        tableBase.createNewTable("a11", 2, 2, "admin");
+        assertTrue(tableBase.containsTable("a11"));
+        tableBase.delete("a11");
+    }
+
+}
